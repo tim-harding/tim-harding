cc39da9de6c7858b257423f3f965b5a217efded4
Tim Harding <Tim@TimHarding.co>
2021-06-17 16:49:29 -0700

    Refactored font feature styles

--- src/App.vue
+++ src/App.vue
@@ -16,26 +16,14 @@ export default defineComponent({
 
 <style lang="scss" module>
 .light {
-	--background: #f2f4f8;
-	--fill: white;
-	--primary: var(--nord6);
-	--secondary: var(--nord5);
-	--tertiary: var(--nord4);
-	--primary-text: var(--nord3);
-	--secondary-text: var(--nord3);
-	--tertiary-text: #7b88a1;
-	--link: var(--nord10);
+	--primary: var(--light-1);
+	--secondary: var(--light-2);
+	--tertiary: var(--light-3);
 }
 
 .dark {
-	--background: #242933;
-	--fill: var(--nord0);
-	--primary: var(--nord1);
-	--secondary: var(--nord2);
-	--tertiary: var(--nord3);
-	--primary-text: var(--nord6);
-	--secondary-text: var(--nord4);
-	--tertiary-text: #abb9cf;
-	--link: var(--nord8);
+	--primary: var(--dark-1);
+	--secondary: var(--dark-2);
+	--tertiary: var(--dark-3);
 }
 </style>
