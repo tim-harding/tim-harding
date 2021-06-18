ba80b3b8de922ae091b209fe7e4bbc0c020743d0
Tim Harding <Tim@TimHarding.co>
2021-06-18 12:08:32 -0700

    Rearranged images

--- src/shared/images.ts
+++ src/shared/images.ts
@@ -6,79 +6,93 @@ export interface ImageInfo {
 
 export const images: ReadonlyArray<ImageInfo> = [
     {
-        name: "absolut_vodka",
-        alt: "Absolut Vodka bottle",
+        name: "hugo_boss",
+        alt: "Hugo Boss watch",
         has4k: true,
     },
-    {
-        name: "aventador",
-        alt: "Lamborghini Aventador sportscar",
-        has4k: true,
-    },
-    {
-        name: "beats",
-        alt: "Beats by Dr. Dre headphones",
-        has4k: true,
-    },
+
     {
         name: "bombay_sapphire",
         alt: "Bombay Sapphire gin bottle",
         has4k: false,
     },
+
     {
-        name: "dior",
-        alt: "Dior perfume bottle",
+        name: "beats",
+        alt: "Beats by Dr. Dre headphones",
         has4k: true,
     },
-    {
-        name: "ferrari_italia",
-        alt: "Ferrari Italia sportscar",
-        has4k: true,
-    },
-    {
-        name: "hugo_boss",
-        alt: "Hugo Boss watch",
-        has4k: true,
-    },
-    {
-        name: "mackie",
-        alt: "Mackie speakers",
-        has4k: false,
-    },
-    {
-        name: "michael_kors",
-        alt: "Michael Kors watch",
-        has4k: true,
-    },
-    {
-        name: "nixon",
-        alt: "Nixon watch",
-        has4k: true,
-    },
-    {
-        name: "ring",
-        alt: "A ring",
-        has4k: false,
-    },
-    {
-        name: "tiffany",
-        alt: "Tiffany perfume",
-        has4k: false,
-    },
-    {
-        name: "tincup",
-        alt: "Tincup whiskey",
-        has4k: true,
-    },
+
     {
         name: "versace",
         alt: "Versace perfume",
         has4k: true,
     },
+
+    {
+        name: "michael_kors",
+        alt: "Michael Kors watch",
+        has4k: true,
+    },
+
+    {
+        name: "ferrari_italia",
+        alt: "Ferrari Italia sportscar",
+        has4k: true,
+    },
+
+    {
+        name: "nixon",
+        alt: "Nixon watch",
+        has4k: true,
+    },
+
+    {
+        name: "absolut_vodka",
+        alt: "Absolut Vodka bottle",
+        has4k: true,
+    },
+
+    {
+        name: "mackie",
+        alt: "Mackie speakers",
+        has4k: false,
+    },
+
+    {
+        name: "aventador",
+        alt: "Lamborghini Aventador sportscar",
+        has4k: true,
+    },
+
+    {
+        name: "dior",
+        alt: "Dior perfume bottle",
+        has4k: true,
+    },
+
     {
         name: "wii",
         alt: "A wiimote controller",
         has4k: true,
+    },
+
+    {
+        name: "tincup",
+        alt: "Tincup whiskey",
+        has4k: true,
+    },
+
+    {
+        name: "tiffany",
+        alt: "Tiffany perfume",
+        has4k: false,
+    },
+
+    {
+        name: "ring",
+        alt: "A ring",
+        has4k: false,
     },
 ]
 
